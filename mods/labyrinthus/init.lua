eb444487a301decab3b15a320ec7ef9cf5604f4d
1248 <1248.kontakt@gmail.com>
2024-02-26 01:27:18 +0100

    Labyrinthus 2.11.1b upload
    hud changes

--- mods/labyrinthus/init.lua
+++ mods/labyrinthus/init.lua
@@ -56,7 +56,7 @@ minetest.register_on_joinplayer(function(player)
 		offset = {x=0, y=10},
 		alignment = {x=1, y=0},
 		number = 0xFFFFFF ,
-		text = "For Minetest 	  :  5.5.0",
+		text = "For Minetest 	  :  5.5.x",
 	})  
 	player:hud_add({
 		hud_elem_type = "text",
@@ -64,7 +64,7 @@ minetest.register_on_joinplayer(function(player)
 		offset = {x=0, y=30},
 		alignment = {x=1, y=0},
 		number = 0xFFFFFF ,
-		text = "Game Version	 :  2.11.1",
+		text = "Game Version	 :  2.11.1b",
 	})
     hud_levels[name] = player:hud_add({
 		hud_elem_type = "text",
